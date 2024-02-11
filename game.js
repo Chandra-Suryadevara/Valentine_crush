9c82166641b820cc4929f18c5b1f9c3c10d5702d
Angad <106414359+Angad58@users.noreply.github.com>
2024-02-11 00:29:07 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-g
+const numRows = 8;
+const numCols = 8;
+const grid = [];
+
+for (let i = 0; i < numRows; i++) {
+    grid[i] = [];
+    for (let j = 0; j < numCols; j++) {
+        // Each cell in the grid is an object containing properties for the icon and position
+        grid[i][j] = {
+            icon: getRandomIcon(),
+            row: i,
+            col: j
+        };
+    }
+}
